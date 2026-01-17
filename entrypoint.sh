9e90e5663839bf3dfa1ea5fb5f6ba0b7763c4abc
Valerii <valerapiliaev@gmail.com>
2026-01-17 20:01:14 +0200

    chore: configure production environment
    - Added DEBUG env variable
    - Set STATIC_ROOT and collectstatic
    - Added WhiteNoise middleware for serving static files
    - Installed and configured gunicorn in Docker
    - Updated docker-compose command for production

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ python manage.py wait_for_db
 
 echo
 python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
 
 echo
 exec "$@"
--- wine_collection/settings.py
+++ wine_collection/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
 
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -141,6 +142,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_ROOT = "/files/media"
 
