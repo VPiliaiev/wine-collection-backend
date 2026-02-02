bb255a07fe2201c7394fdd1322501f35778a0218
Valerii <valerapiliaev@gmail.com>
2026-02-02 12:39:17 +0200

    feat: add list endpoints for country, category, mood, purpose and wine type

--- wine/urls.py
+++ wine/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
-from wine.views import WineListView, WineDetailView
+from wine.views import (
+    WineListView,
+    WineDetailView,
+    CountryListAPIView,
+    CategoryListAPIView,
+    MoodListAPIView,
+    PurposeListAPIView,
+    WineTypeListAPIView,
+)
 
 urlpatterns = [
     path("wines/", WineListView.as_view(), name="wine-list"),
     path("wines/<int:pk>/", WineDetailView.as_view(), name="wine-detail"),
+    path("countries/", CountryListAPIView.as_view(), name="country-list"),
+    path("categories/", CategoryListAPIView.as_view(), name="category-list"),
+    path("moods/", MoodListAPIView.as_view(), name="mood-list"),
+    path("purposes/", PurposeListAPIView.as_view(), name="purpose-list"),
+    path("wine-types/", WineTypeListAPIView.as_view(), name="wine-type-list"),
 ]
 app_name = "wine"
