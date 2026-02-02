57e4d47be4166c4f67e77b6802105171d9588715
Valerii <valerapiliaev@gmail.com>
2026-02-02 12:38:00 +0200

    feat: switch wine filters to id based

--- wine/filters.py
+++ wine/filters.py
@@ -1,15 +1,31 @@
 import django_filters
-from wine.models import Wine
+from wine.models import Wine, Mood, Category, Purpose, Country, WineType
 
 
 class WineFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
-    mood = django_filters.AllValuesMultipleFilter(field_name="moods__name")
     min_price = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
     max_price = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
-    category = django_filters.AllValuesMultipleFilter(field_name="category__name")
-    purpose = django_filters.AllValuesMultipleFilter(field_name="purpose__name")
-    country = django_filters.AllValuesMultipleFilter(field_name="country__name")
+    mood = django_filters.ModelMultipleChoiceFilter(
+        field_name="moods",
+        queryset=Mood.objects.all(),
+    )
+    category = django_filters.ModelMultipleChoiceFilter(
+        field_name="category",
+        queryset=Category.objects.all(),
+    )
+    purpose = django_filters.ModelMultipleChoiceFilter(
+        field_name="purpose",
+        queryset=Purpose.objects.all(),
+    )
+    country = django_filters.ModelMultipleChoiceFilter(
+        field_name="country",
+        queryset=Country.objects.all(),
+    )
+    wine_type = django_filters.ModelMultipleChoiceFilter(
+        field_name="wine_type",
+        queryset=WineType.objects.all(),
+    )
 
     class Meta:
         model = Wine
@@ -21,4 +37,5 @@ class WineFilter(django_filters.FilterSet):
             "category",
             "purpose",
             "country",
+            "wine_type",
         ]
